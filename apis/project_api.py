4d79f8c5df4c4e76adf91c79bf482b570b92da20
fjqken <469965307@qq.com>
2023-06-16 17:29:28 +0800

    随便搞搞

--- apis/project_api.py
+++ apis/project_api.py
@@ -2,7 +2,7 @@ import json
 
 from flask import Blueprint, jsonify
 from flask import request
-from common.database import db, project
+from common.database import db, project, user_fung
 
 project_api = Blueprint("project_api", __name__)
 
@@ -13,6 +13,7 @@ def creat_project():
     js_data = json.loads(data)
     print(js_data)
     project_obj = project(project_name=js_data['project_name'], address=js_data['address'])
+    project_obj.create_user = user_fung.query.filter(user_fung.id == 1).first()
     db.session.add(project_obj)
     db.session.commit()
     return jsonify({
@@ -22,10 +23,13 @@ def creat_project():
 
 @project_api.route("/project_api/select_list", methods=['POST'])
 def select_project():
-    project_list = project.query.filter().all()
+    user_obj = user_fung.query.filter(user_fung.id == 1).first()
+    for project1 in user_obj.project:
+        print(project1.project_name)
+    # project_list = project.query.filter().all()
     # db.session.add(project_list)
-    for i in project_list:
-        print(i.project_name)
+    # for i in project_list:
+    #     print(i.project_name)
     return jsonify({
         'code': 20000, 'msg': '查询项目成功', 'success': True
     })
